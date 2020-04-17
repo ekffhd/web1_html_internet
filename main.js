df0d20c6d003f859746836c7db9c3429ed1d748a
shelly <shelltlove1@naver.com>
2020-04-18 04:56:51 +0900

    nodejs : redirect & GET / POST

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
-
     var template = `
                 <!doctype html>
                     <html>
@@ -12,8 +12,9 @@ function templateHTML(title, list, body){
                     <meta charset="utf-8">
                     </head>
                     <body>
-                    <h1><a href="/">WEB</a></h1>
+                    <h1><a href="/">WEB2</a></h1>
                      ${list}
+                     <a href="/create">create</a>
                      ${body}
                      
                     </body>
@@ -58,7 +59,7 @@ var app = http.createServer(function(request,response){
             });
 
 
-        } else{
+        } else {
             fs.readdir(dataFolder, function(error, filelist) {
                 fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
 
@@ -69,12 +70,53 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
+    } else if(pathname === '/create'){
+        fs.readdir(dataFolder, function(error, filelist){
 
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+                <form action="http://localhost:3000/create_process" method="post">
+                    <p> <input type="text" name="title" placeholder="title"/></p>
+                    <p>
+                        <textarea name="description" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>
+            `);
+
+            response.writeHead(200);
+            response.end(template);
+
+        });
+
+    } else if(pathname === '/create_process'){
+        var body = '';
+
+        request.on('data', function(data) {
+            body += data;
+        });
+
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+
+            fs.writeFile(`data/${title}`, description, 'utf8', (err)=>{
+                response.writeHead(302, // redirec
+                    {
+                        Location: `/?id=${title}`
+                    });
+                response.end();
+            })
+        });
 
 
     } else {
         response.writeHead(404);
-        response.end('Not found');
+        response.end('404:Not found');
     }
 
 
